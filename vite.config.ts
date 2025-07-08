f4f5ab30a3f7eecf60203ffe7c0759fd2c9fce29
Rafinur Mursalimov <ArtArtistRM@yandex.ru>
2025-07-08 18:16:09 +0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/RafinurM/todos',
   plugins: [react()],
   resolve: {
     alias: {
